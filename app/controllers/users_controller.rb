1646ea7369213833924e97f4bf5ca7db50499cc8
reina.kurogi <kuro.reian.8@gmail.com>
2019-12-07 07:12:02 +0000

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,15 @@
 class UsersController < ApplicationController
+  
   def new
     @user = User.new
   end
   
-  def show
-    @user = User.find(params[:id])
-  end
-
   def create
     @user = User.new(user_params)
     
     if @user.save
       flash[:success] = "ユーザを登録しました。"
-      redirect_to @user
+      redirect_to controller: :tasks, action: :index
     else
       flash[:danger] = "ユーザの登録に失敗しました。"
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
-  get 'users/new'
-  get 'users/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #root to: "tasks#index"
-  root to: "toppages#index"
+  
+  root to: "tasks#index"
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   resources :tasks
   
